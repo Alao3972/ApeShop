3c3cd043d7a5defaaea03d2ca34dbeae96d0d7b4
Alao3972 <andylao409@hotmail.com>
2017-09-29 14:45:12 -0400

    Finished Sign up layout

--- src/main/java/com/example/andy/apeshop/SignUp.java
+++ src/main/java/com/example/andy/apeshop/SignUp.java
@@ -4,14 +4,57 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class SignUp extends AppCompatActivity {
 
+    Spinner countrySpinner;
+    Spinner provinceSpinner;
+    ArrayAdapter<CharSequence> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //country Spinner
+        countrySpinner = (Spinner) findViewById(R.id.countrySpinner);
+        adapter = ArrayAdapter.createFromResource(this, R.array.countryArray,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        countrySpinner.setAdapter(adapter);
+        countrySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(),parent.getItemAtPosition(position)+" selected", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        //province Spinner
+        provinceSpinner = (Spinner) findViewById(R.id.provinceSpinner);
+        adapter = ArrayAdapter.createFromResource(this, R.array.provinceArray,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        provinceSpinner.setAdapter(adapter);
+        provinceSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(),parent.getItemAtPosition(position)+" selected", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
+
     /**
      * Button to home page
      * @param view
@@ -20,4 +63,5 @@ public class SignUp extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
 }
