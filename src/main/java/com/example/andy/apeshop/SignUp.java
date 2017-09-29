5db1090d5cbff71eecee9c0c34c074b2c57ee526
Alao3972 <andylao409@hotmail.com>
2017-09-29 17:37:00 -0400

    Finished Sign up Validations

--- src/main/java/com/example/andy/apeshop/SignUp.java
+++ src/main/java/com/example/andy/apeshop/SignUp.java
@@ -6,10 +6,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class SignUp extends AppCompatActivity {
+
+    EditText firstName;
+    EditText lastName;
+    EditText email;
+    EditText password;
+    EditText address;
+    Button signUpButton;
 
     Spinner countrySpinner;
     Spinner provinceSpinner;
@@ -27,7 +37,7 @@ public class SignUp extends AppCompatActivity {
         countrySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getBaseContext(),parent.getItemAtPosition(position)+" selected", Toast.LENGTH_LONG).show();
+
             }
 
             @Override
@@ -44,7 +54,7 @@ public class SignUp extends AppCompatActivity {
         provinceSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getBaseContext(),parent.getItemAtPosition(position)+" selected", Toast.LENGTH_LONG).show();
+
             }
 
             @Override
@@ -53,7 +63,69 @@ public class SignUp extends AppCompatActivity {
             }
         });
 
+        firstName = (EditText)findViewById(R.id.firstNameText);
+        lastName = (EditText)findViewById(R.id.lastNameText);
+        email = (EditText)findViewById(R.id.emailText);
+        password = (EditText)findViewById(R.id.passwordText);
+        address = (EditText)findViewById(R.id.addressText);
+
+        signUpButton = (Button)findViewById(R.id.signUpButton);
+        signUpButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String newFirstName = firstName.getText().toString();
+                String newLastName = lastName.getText().toString();
+                String newEmail = email.getText().toString();
+                String newPassword = password.getText().toString();
+                String newAddress = address.getText().toString();
+                String newCountry = countrySpinner.getSelectedItem().toString();
+                String newProvince = provinceSpinner.getSelectedItem().toString();
+
+                if (newFirstName.length()==0){
+                    firstName.requestFocus();
+                    firstName.setError("First Name cannot be Empty");
+                }
+                else if (newLastName.length()==0){
+                    lastName.requestFocus();
+                    lastName.setError("Last Name cannot be empty");
+                }
+                else if (newEmail.length()==0){
+                    email.requestFocus();
+                    email.setError("Email cannot be empty");
+                }
+                else if (newPassword.length()==0){
+                    password.requestFocus();
+                    password.setError("Password cannot be empty");
+                }
+                else if (newAddress.length()==0){
+                    address.requestFocus();
+                    address.setError("Address cannot be empty");
+                }
+                else if (newCountry.matches("Select")){
+                    countrySpinner.requestFocus();
+                    Toast.makeText(getBaseContext(),"Country Not Selected", Toast.LENGTH_LONG).show();
+                }
+                else if (newProvince.matches("Select")){
+                    provinceSpinner.requestFocus();
+                    Toast.makeText(getBaseContext(),"Province Not Selected", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    startActivity(new Intent(SignUp.this, Login.class));
+                }
+            }
+        });
+
+
+
     }
+
+    public void signUp(View view){
+
+    }
+
+
+
+
 
     /**
      * Button to home page
